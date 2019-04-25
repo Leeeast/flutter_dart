3c2fab0f2daca2b752d48940bbac0f5c2e2f5ed6
lidongliang <dongfang369>
2019-04-25 23:52:51 +0800

    dartji基础

--- list_set.dart
+++ list_set.dart
@@ -4,13 +4,14 @@
 main(List<String> args) {
   /// 定义list
   List<String> list = List(); // 不推荐
-  var ls = [1,2,3,4,5,6]; // 推荐
+  var ls = [1, 2, 3, 4, 5, 6]; // 推荐
 
-  ls.forEach((item){print(item);});
+  ls.forEach((item) {
+    print(item);
+  });
   print('*' * 100);
   ls.add(10); // 添加元素， 和Java集合操作类似
   ls.forEach((item) => print(item));
-
   print('*' * 100);
   ls.remove(2); // 删除元素
   for (var item in ls) {
@@ -20,18 +21,22 @@ main(List<String> args) {
 
   /// 链式操作
   print('--' * 50);
-  ls..add(30)
-    ..add(20)
-    ..add(40);
+  ls..add(30)..add(20)..add(40);
   ls.forEach((item) => print(item));
+  ls.sort((left, right) => (left.toString().compareTo(right.toString())));
 
   /// 元素不可重复列表
-  Set<String> set1 = {'1','2','3','3','2'};
+  Set<String> set1 = {'1', '2', '3', '3', '2'};
   print(set1); // 会去除重复元素
 
   set1.forEach((item) => print(item));
   print('-' * 80);
-  set1..add('123')
-      ..add('456');
-  set1.forEach((item){print(item);});
+  set1..add('123')..add('456');
+  set1.forEach((item) {
+    print(item);
+  });
+
+  Set set = new Set();
+  set..add('b')..add('a')..add(2)..add("Hello world！");
+  print(set);
 }
--- map_emnu.dart
+++ map_emnu.dart
@@ -1,20 +1,16 @@
 import 'dart:convert';
+
 /// map
 ///
 ///
 
 main(List<String> args) {
   /// 定义map
-  var map1 = <String,dynamic>{
-    'name': 'zeno',
-    'age': 20,
-    'weight': 120
-  };
+  var map1 = <String, dynamic>{'name': 'zeno', 'age': 20, 'weight': 120};
   print(map1);
   var jsonStr = jsonEncode(map1);
   print(jsonStr);
 
   map1['address'] = 'hu nan chang sha';
-  map1.forEach((key,val)=> print("$key : $val"));
-  
+  map1.forEach((key, val) => print("$key : $val"));
 }
